7e71c00ee50489b6ffaf5caa94a0b9afa406aafb
Jahidul islam Sabuz <79420197+coderjahidul@users.noreply.github.com>
2024-03-24 09:45:23 +0000

    save variable code

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 567289;
+let accountEmail = "jahidul@gmail.com";
+var accountPassword = "41427677";
+accountCity = "Laksham";
+let accountState;
+
+
+// accountId = 2; // not allowed
+accountEmail = "sobuz@gmail.com";
+accountPassword = "123456";
+accountCity = "Cumilla";
+console.log(accountId);
+/*
+Prefer not use var
+because of issus in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
