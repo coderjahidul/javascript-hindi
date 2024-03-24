41d3111b8d1d0c7b1cf6b52ccb14c46b674e1121
Jahidul islam Sabuz <79420197+coderjahidul@users.noreply.github.com>
2024-03-24 17:46:00 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be hight
+
+console.log("jahidul");
+
+let name = "jahidul" // string data types
+let age = 18 // number data types
+let isLoggedIn = false // boolean data types
+
+// number => 2 power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value / null
+// undefined => 
+// symbol => unique
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
