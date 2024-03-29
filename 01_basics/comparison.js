1f6cb7524e8293e33bec43567adea1c6e2459004
coderjahidul <sobuz0349@gmail.com>
2024-03-29 16:45:24 +0600

    Discuss Comparison Operations. and Discuss Dataile Comparison right way and wrong way

---
+++ 01_basics/comparison.js
@@ -0,0 +1,29 @@
+// Comparison Operations. It is right way to compare. 
+// console.log(2 > 1); // true
+// console.log(2 >= 1); // true
+// console.log(2 < 1); // false
+// console.log(2 <= 1); // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); // true
+// console.log(2 === 1); // false
+// console.log(2 !== 1); //true
+
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+
+// avoid this comparison. It is wrong way to compare. please write clean code to do comparison.
+// console.log(null >0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+// console.log(null < 0); // false
+// console.log(null <= 0); // true
+// console.log(null != 0); // true
+// console.log(null !== 0); // true
+
+// console.log(undefined > 0); // false
+// console.log(undefined == 0); // false
+// console.log(undefined >= 0); // false
+// console.log(undefined < 0); // false
+// console.log(undefined <= 0); // false
+// console.log(undefined != 0); // true
+// console.log(undefined !== 0); // true
