5b24cdd257c75729ff39323a826bbfdb18060b61
coderjahidul <sobuz0349@gmail.com>
2024-03-29 15:21:29 +0600

    datatypes update

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,19 +1,19 @@
 // Conversion Operations
 // diclare a String type variable
-let score = "jahidul islam";
+// let score = "jahidul islam";
 
 // Check variable type
-console.log(typeof(score));
-console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof score);
 
 // Convert string type to number type
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
 
 // Convert number type to string type
-let valueInString = String(valueInNumber);
-console.log(typeof valueInString);
-console.log(valueInNumber);
+// let valueInString = String(valueInNumber);
+// console.log(typeof valueInString);
+// console.log(valueInNumber);
 
 /* 
 1. if diclared variable is let number = "12abc"; 
@@ -30,10 +30,11 @@ and Convert string type to number type or number type to string type then it wil
 6. if diclared variable is let nubmebe = 33; and Convart string type to number type or number type to string type then it will give 33.
 */ 
 
-let isLoggedIn = undefined;
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
-console.log(booleanIsLoggedIn);
+// diclare a Boolean type variable
+// let isLoggedIn = undefined;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /*
 1. if diclared variable is let isLoggedIn = 0; and Convert boolean type then it will give false. and 1 will give true.
@@ -43,4 +44,86 @@ console.log(booleanIsLoggedIn);
 3. if diclared variable is let isLoggedIn = null; and Convert boolean type then is will give false.
 and undefined will give false.
 */
+
+// let number = "12abc";
+// console.log(typeof(number));
+// console.log(number);
+// console.log('\n');
+
+// let valueInStringone = String(number);
+// console.log(typeof(valueInString));
+// console.log(valueInString);
+// console.log('\n'); 
+
+// let valueInNumberone = Number(number);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// let numberone = "jahidul islam";
+// let valueInBoolean = Boolean(numberone);
+// console.log(typeof(valueInBoolean));
+// console.log(valueInBoolean);
+
+
+/****************************** Operations  ****************************/
+// Negative value
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// Adition Operator
+// console.log(2+2);
+
+// Subtraction Operator
+// console.log(2-2);
+
+// Multiplication Operator
+// console.log(2*2);
+
+// Division Operator
+// console.log(5/2);
+
+// raises to power Operator
+// console.log(3**2);
+
+// modulus operator
+// console.log(3%2);
+
+// let str1 = "hello";
+// let str2 = " jahidul";
+// let str3  = str1 + str2;
+// console.log(str3);
+
+console.log("1" + 2);
+// if console.log("1" + 2); then it will give string value "12" and console.log(1 + 2); then it will give number value 3.
+  
+console.log(1 + "2"); 
+// if console.log(1 + "2"); then it will give string value "12" and console.log(1 + 2); then it will give number value 3.
+
+console.log("1" + (2 + 2));
+// if console.log("1" + (2 + 2)); then it will give string value "14" and console.log(1 + 2 + 2); then it will give number value 5 and console.log("1" + 2 + 2); then it will give string value "122".
+
+console.log(1 + 2 + "2");
+// if console.log(1 + 2 + "2"); then it will give string value "32" and console.log((1 + 2) + "2"); then it will give string value "32".
+
+// increment operator
+let num = 3; // assign num value 3
+num = num + 1; // add 1 to num value
+num += 1; // add 1 to num value
+num++; // add 1 to num value
+console.log(num);
+
+// decrement operator
+let num1 = 6; // assign num1 value 6
+num1 = num1 - 1; // sub 1 from num1 value
+num1 -=1; // sub 1 from num1 value
+num1--; // sub 1 from num1 value
+console.log(num1);
+
+
+
+
+
+
+
 
