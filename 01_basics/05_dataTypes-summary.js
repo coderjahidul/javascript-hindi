0226953fb295f25d9cc4ab139cb76c7d3f820276
coderjahidul <sobuz0349@gmail.com>
2024-03-29 22:29:48 +0600

    Explaine DataTypes Summary

---
+++ 01_basics/05_dataTypes-summary.js
@@ -0,0 +1,87 @@
+// javaScript is a dynamic Language. It can change its data type.
+// callbyvalue and callbyreference
+// Primitive Data Types Summary 
+// 01. string
+// 02. number
+// 03. boolean
+// 04. null
+// 05. undefined
+// 06. symbol
+// 07. bigint
+
+// const score = 100; // number
+// const scoreValue = 100.5; // number
+// const isLoggedIn = false; // boolean
+// const outsideTemp = null; // object
+// let userEmail; // undefined
+
+// dataType symbol is unique. This two symbol value is not equal. 
+// const id = Symbol('123'); // symbol
+// const anotherId = Symbol('123'); // symbol
+// console.log(id === anotherId); // false
+
+//dataType bigint
+// const bitNumber = 2324154496545n; // bigint
+// console.log(typeof bitNumber);
+
+
+
+
+
+
+// Reference Data Types Summary (Non Primitive)
+// 01. object
+// 02. function
+// 03. array
+
+// dataType object
+let user = {
+    name: "jahidul islam",
+    age: 26,
+    address: "laksham",
+    isMarried: false,
+    isaCometead: true,
+    girlfrindsName: ["Rimi", "Choity", "Salma", "Sowon"],
+    exGrilfrindsName: "Priya",
+}
+
+// dataType function
+let myfunction = function(){
+    console.log("jahidul islam");
+}
+
+myfunction();
+
+// dataType array
+const heros = ['marvel', 'ironman', 'superman']; // array
+
+console.log(typeof user);
+console.log(typeof myfunction);
+console.log(typeof heros);
+
+
+
+
+// css is not a language. It is a style sheet.
+// html is not a language. It is a markup language.
+// JavaScript is a language. it is a programming language.
+// jQuery is a library. it is a javascript library.
+// php is a language. it is a server side programming language.
+// mysql is a database. it is a relational database.
+// laravel is a framework. it is a PHP framework.
+// angular is a framework. it is a javascript framework.
+// react is a framework. it is a javascript framework.
+// vue is a framework. it is a javascript framework.
+// node is a framework. It is a javascript framework.
+// python is a language. It is a server side programming language.
+// c++ is a language. It is a server side programming language.
+// c# is a language. It is a server side programming language.
+// java is a language. It is a server side programming language.
+// swift is a language. It is a server side programming language.
+// ruby is a language. It is a server side programming language.
+// go is a language. It is a server side programming language.
+// kotlin is a language. It is a server side programming language.
+// scala is a language. It is a server side programming language.
+// typescript is a language. It is a server side programming language.
+// rust is a language. It is a server side programming language.
+// deno is a language. It is a server side programming language.
