a0244591afe676eafe381d71f5dff733e0a74b24
coderjahidul <sobuz0349@gmail.com>
2024-03-31 23:21:13 +0600

    explaine string dataile

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,9 @@
+const name = "jahidul islam";
+const repoCount = 50;
+
+console.log(name + " " + repoCount + " " + "Value"); // The is old way
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // The is new way
+
+const gameName = new String("Supperman");
+console.log(gameName[4]);
