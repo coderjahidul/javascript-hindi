17a0148c784496ab06132b25df9cbc2c70f45e5c
coderjahidul <sobuz0349@gmail.com>
2024-07-30 18:02:28 +0600

    modufy strings function

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -7,11 +7,54 @@
 // const gameName = new String("Supperman");
 // console.log(gameName[4]);
 
-const name = "jahidul islam";
-const repoCount = 70;
-console.log(name + " " + repoCount + " " + "Value"); // The is old way
+// const name = "jahidul islam";
+// const repoCount = 70;
+// console.log(name + " " + repoCount + " " + "Value"); // The is old way
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // The is new way
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // The is new way
 
-const gameName = new String("Supperman");
-console.log(gameName[0]);
+// const gameName = new String("Supperman");
+// console.log(gameName[0]);
+
+// Creating Strings
+// const string1 = "A string primitive";
+// const string2 = `Also a string primitive`;
+// const string3 = `Yet another string primitive`;
+// console.log(string1, string2, string3);
+
+// const string4 = new String("A string object");
+// console.log(string4);
+
+// Character Access
+// "jahidul".charAt(1);
+// "jahidul".charCodeAt(1);
+// "jahidul".indexOf("u");
+// console.log("jahidul".charAt(0)); // gives value of j
+// console.log("jahidul"[2]); // gives value of h
+// console.log("jahidul".charCodeAt(1)); // gives value of 97
+// console.log("jahidul".indexOf("u")); // gives value of 5
+
+// Comparison Strings
+// const a = "a";
+// const b = "b";
+// if(a < b){
+//     console.log(`${a} is less than ${b}`);
+// }else if(a > b){
+//     console.log(`${a} is greater than ${b}`);
+// }else {
+//     console.log(`${a} and ${b} are equal`);
+// }
+
+// function areEqualCaseInsensitive(str1, str2){
+//     return str1.toUpperCase() === str2.toUpperCase();
+// }
+
+// console.log(areEqualCaseInsensitive("Jahidul", "jahidul"));
+
+const areEqualInUpperCase = (str1, str2) => str1.toUpperCase() === str2.toUpperCase();
+const areEqualInLowerCase = (str1, str2) => str1.toLowerCase() === str2.toLowerCase();
+
+areEqualInUpperCase("ß", "ss");
+areEqualInLowerCase("i", "I");
+console.log(areEqualInUpperCase("ß", "ss"));
+console.log(areEqualInLowerCase("i", "I"));
