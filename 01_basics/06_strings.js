f97802beacd259e532b8485b32cf50fbdfe2853e
coderjahidul <sobuz0349@gmail.com>
2024-07-30 23:33:30 +0600

    modify strings function

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -51,10 +51,37 @@
 
 // console.log(areEqualCaseInsensitive("Jahidul", "jahidul"));
 
-const areEqualInUpperCase = (str1, str2) => str1.toUpperCase() === str2.toUpperCase();
-const areEqualInLowerCase = (str1, str2) => str1.toLowerCase() === str2.toLowerCase();
+// const areEqualInUpperCase = (str1, str2) => str1.toUpperCase() === str2.toUpperCase();
+// const areEqualInLowerCase = (str1, str2) => str1.toLowerCase() === str2.toLowerCase();
 
-areEqualInUpperCase("ß", "ss");
-areEqualInLowerCase("i", "I");
-console.log(areEqualInUpperCase("ß", "ss"));
-console.log(areEqualInLowerCase("i", "I"));
+// areEqualInUpperCase("ß", "ss");
+// areEqualInLowerCase("i", "I");
+// console.log(areEqualInUpperCase("ß", "ss"));
+// console.log(areEqualInLowerCase("i", "I"));
+
+// const areEqual = (str1, str2, locale = "en-US") => str1.localeCompare(str2, locale, {sensitivity: "accent"}) === 0;
+
+// console.log(areEqual("ß", "ss", "de")); // false
+// console.log(areEqual("ı", "I", "tr")); // true
+
+// const strPrim = "foo"; // A literal is a string primitive
+// console.log(typeof strPrim); // "string"
+
+// const strPrim2 = String(1);
+// console.log(typeof strPrim2); // "string"
+
+// const strPrim3 = String(true);
+// console.log(typeof strPrim3); // "string"
+
+// const strObj = new String(strPrim);
+// console.log(typeof strObj); // "object"
+
+// const s1 = "2 + 2"; // creates a string primitve
+// console.log(eval(s1)); // returns the number 4
+
+// const s2 = new String("2 + 2"); // creates a String object
+// console.log(eval(s2)); // returns the string "2 + 2"
+
+// console.log(eval(s2.valueOf())); // returns the number 4
+
+
