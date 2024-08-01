ee98290331c59b33d04181c28240aa9ceb1fda3a
coderjahidul <sobuz0349@gmail.com>
2024-08-01 22:54:23 +0600

    Explain javascript Math function

--- 01_basics/07_number_and_maths.js
+++ 01_basics/07_number_and_maths.js
@@ -18,3 +18,29 @@
 // console.log(hundreds.toLocaleString('en-IN')); // Return value the 10,00,000
 // console.log(hundreds.toLocaleString()); // Return value the 1,000,000
 
+//+++++++++++++++++++++++ Maths ++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // Return the Positive value 4
+// console.log(Math.round(4.3)); // Rutern the Round Value 4
+// console.log(Math.round(4.7)); // Return the Round Value 5
+// console.log(Math.ceil(4.2)); // Return the Biger or Top Number Value 5
+// console.log(Math.floor(4.2)); // Return the lower or bottom Number Value 4
+// console.log(Math.sqrt(25)); // Return the Squaire Number Value 5
+// console.log(Math.min(9,8,5,7,6,4,2,9,3)); // Return the Minimam Number Value 2
+// console.log(Math.max(9,8,5,7,6,4,2,9,3)); // Return the Max Number Value 9
+// console.log(Math.random()); // Return the Rendom Number Value
+// console.log(Math.random()*10); // Return the Rendom Numbar Value 0 to 10 lenth
+// console.log((Math.random()*5) + 1); // Return the Rendom Number Value 1 to 5 lenth
+
+// // Create a formola
+// const min = 10;
+// const max = 20;
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+const min = 15;
+const max = 25;
+
+console.log(Math.ceil(Math.random() * (max - min + 1)) + min);
+
+
