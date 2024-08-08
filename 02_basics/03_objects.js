37f9ee79eb2ee0708f831f70897ecb1ab8c46259
coderjahidul <sobuz0349@gmail.com>
2024-08-08 14:44:58 +0600

    update object function

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,17 +3,17 @@
 
 //Object Literals
 
-const mySym = Symbol("key1");
-const jsUser = {
-    name: "jahidul",
-    "full name": "Jahidul Islam Sabuz",
-    [mySym]: "mykey1",
-    age: 26,
-    location: "Uttora, Dhaka",
-    email: "jahidul@imjol.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Sunday", "Friday"]
-}
+// const mySym = Symbol("key1");
+// const jsUser = {
+//     name: "jahidul",
+//     "full name": "Jahidul Islam Sabuz",
+//     [mySym]: "mykey1",
+//     age: 26,
+//     location: "Uttora, Dhaka",
+//     email: "jahidul@imjol.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Monday", "Sunday", "Friday"]
+// }
 
 // console.log(jsUser.email); // Return the value like jahidul@imjol.com
 // console.log(jsUser["email"]); // Return the value like jahidul@imjol.com
@@ -24,13 +24,76 @@ const jsUser = {
 // jsUser.email = "jahidul@microsoft.com"; 
 // console.log(jsUser);
 
-jsUser.greeting = function(){
-    console.log("Hello js user");
-}
+// jsUser.greeting = function(){
+//     console.log("Hello js user");
+// }
 
-console.log(jsUser.greeting());
+// console.log(jsUser.greeting());
 
-jsUser.greetingTwo = function(){
-    console.log(`Hello js User, ${this["full name"]}`);
+// jsUser.greetingTwo = function(){
+//     console.log(`Hello js User, ${this["full name"]}`);
+// }
+// console.log(jsUser.greetingTwo());
+
+// Real life Object
+// const car = {
+//     type: "Fiat",
+//     model: "500",
+//     color: "white",
+// }
+
+// console.log(car.model);
+
+// Create JavaScript Object. Spaces and line breaks are not important. An object initializer can span multiple lines
+// const user = {
+//     firstName: "Jahidul Islam",
+//     lastName: "Sabuz",
+//     age: 26,
+//     location: "Uttora, Dhaka",
+//     email: "jahidul@imjol.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Monday", "Sunday", "Friday"],
+// }
+// console.log(user["location"]);
+
+// Create JavaScript Object This example creates an empty JavaScript object, and then adds properties
+// const user2 = new Object();
+
+// Add properties
+// user2.fristName = "Jahidul Islam";
+// user2.lastName = "Sabuz";
+// user2.age = 26;
+// user2.location = "Uttora, Dhaka";
+// user2.email = "jahidul@imjol.com";
+// user2.isLoggedIn = false;
+// user2.lastLoginDays = ["Monday", "Sunday", "Friday"];
+// user2.fullName = function(){
+//     return this.fristName + " " + this.lastName;
+// }
+
+// Object.freeze(user2);
+// console.log(user2.email);
+// user2.email = "imJol@example.com";
+// console.log(user2["fullName"]());
+
+
+
+
+// Create JavaScript Object
+const person = {
+    firstName: "Jahidul Islam",
+    lastName: "Sabuz",
+    age: 26,
+    location: "Uttora, Dhaka",
+    email: "jahidul@imjol.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Sunday", "Friday"],
+    fullName: function(){
+        return this.fristName + " " + this.lastName;
+    }
 }
-console.log(jsUser.greetingTwo());
+const x = person;
+x.age = 27;
+console.log(person);
+
+
