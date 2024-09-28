d21bc91c434fa840451b99263bc60d6e6d2413ce
coderjahidul <sobuz0349@gmail.com>
2024-09-29 01:51:47 +0600

    explain javascript while loop and do while loop function

---
+++ 05_iterations/01_while_loop.js
@@ -0,0 +1,21 @@
+// While Loop function 
+let myarray = ["jahidul", "salma", "rimi"];
+let i = 0;
+// while(i<myarray.length){
+//     console.log(myarray[i]);
+//     i++;
+// }
+
+// let j = 0;
+// while(j<=20){
+//     console.log(`This Value is a ${j}`);
+//     j += 1;
+// }
+
+
+// do while loop
+let k = 11;
+do{
+    console.log(`This Value is a ${k}`);
+    k += 1;
+}while(k<=10);
