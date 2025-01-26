15d423937c3089bbb7558eef55d83e9233c7159a
coderjahidul <sobuz0349@gmail.com>
2025-01-26 12:10:02 +0600

    create project 2 with javaScript

---
+++ project-two/custom.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const height = parseInt(document.querySelector('#height').value);
+
+  const weight = parseInt(document.querySelector('#weight').value);
+  console.log(weight);
+  console.log(height);
+
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+    // Determine BMI category
+    if (bmi < 18.6) {
+      results.innerHTML = `<span>Under Weight = ${bmi}</span>`;
+    } else if (bmi >= 18.6 && bmi <= 24.9) {
+      results.innerHTML = `<span>Normal Range = ${bmi}</span>`;
+    } else if (bmi > 24.9) {
+      results.innerHTML = `<span>Overweight = ${bmi}</span>`;
+    }
+  }
+});
